41c53ab46f6dec92536e4239d45f8e751c52e875
Marta Santana <dra.martasantana@hotmail.com>
2021-04-13 17:40:19 -0300

    Modifiquei

--- valida.php
+++ valida.php
@@ -4,7 +4,7 @@
     $senha = $_POST["matricula"];
 
     if($login == "dra.martasantana@hotmail.com" && $senha == "1913961396") {
-        header('location:index.php?erro=1');
+        header('location:index.php?erro=0');
     };
 
     echo'<h1> Login realizado com sucesso!</h1>'
